f1bc32a179c07652b3d303925a7b9a4581edc4c4
Gustavo Amorim <guh.contato.work@outlook.com.br>
2021-08-27 20:05:48 -0300

    Vue: Third Class

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  <h1>Hello</h1>
+  <h1>{{ person }}</h1>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue"
 
-export default defineComponent({})
+export default defineComponent({
+  data() {
+    return {
+      person: ''
+    }
+  }
+})
 </script>
