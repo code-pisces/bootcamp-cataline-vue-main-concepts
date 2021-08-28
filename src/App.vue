d956c734164545db7a8971a0e06c785591d34263
Gustavo Amorim <guh.contato.work@outlook.com.br>
2021-08-27 21:50:20 -0300

    Vue: Seventh Class

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,60 @@
 <template>
-  <h1>{{ count }}</h1>
+  <h1>Seus todos</h1>
+  <ul>
+    <li v-for="todo in doneTodos" :key="todo.text">
+      {{ todo.text }}
+    </li>
+  </ul>
 
-  <button @click="count++">Incrementar</button>
-
+  <button @click="markAllLikeComplete">Marcar todos como concluídos</button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue"
 
+interface Todo {
+  text: string
+  done: boolean
+}
+
 export default defineComponent({
   data() {
     return {
-      count: 0
+      todos: [] as Todo[]
+    }
+  },
+
+  computed: {
+    doneTodos(): Todo[] {
+      const checkedToDos = this.todos.filter((value) => value.done === true)
+
+      return checkedToDos
     }
   },
   watch: {
-    count(newValue, oldValue) {
-      console.log(oldValue)
-      console.log(newValue)
+    todos(newValue: Todo[]) {
+      const isFinished = newValue.some(({ done }) => !done)
+
+      if (isFinished) {
+        alert("Concluído")
+      }
+    }
+  },
+  created() {
+    this.todos = [
+      { text: "Estudar Typescript", done: true },
+      { text: "Lavar os pratos", done: false },
+      { text: "Aprender Nuxt.js", done: true }
+    ]
+  },
+  methods: {
+    markAllLikeComplete() {
+      this.todos = this.todos.map(({ text }) => {
+        return {
+          text,
+          done: true
+        }
+      })
     }
   }
 })
