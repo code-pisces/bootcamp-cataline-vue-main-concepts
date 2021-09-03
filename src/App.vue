a420e9c54afdc1dd7770881b1390cbf62c8fd716
Gustavo Amorim <guh.contato.work@outlook.com.br>
2021-09-03 20:48:16 -0300

    Diretiva: v-model

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,36 @@
 <template>
-  <ul>
-    <li v-for="product in products" :key="product.id">
-      {{product.name}}
-    </li>
-  </ul>
+  <form>
+    <input type="text" placeholder="Nome" v-model.lazy="form.name" />
+
+    <input type="number" placeholder="Idade" v-model.number="form.age" />
+
+    <textarea
+      rows="2"
+      placeholder="Descrição"
+      v-model.trim="form.message"
+    ></textarea>
+
+    <input type="checkbox" v-model="form.newsletter" /> Newsletter <br />
+
+    <input type="radio" name="color" value="vermelho" v-model="form.color" />
+    Vermelho
+    <input type="radio" name="color" value="verde" v-model="form.color" /> Verde
+    <input type="radio" name="color" value="azul" v-model="form.color" /> Azul
+
+    <select v-model="form.fruit">
+      <option disabled selected>Selecione uma fruta</option>
+      <option value="uva">Uva</option>
+      <option value="banana">Banana</option>
+      <option value="morango">Morango</option>
+    </select>
+  </form>
+
+  <p>Nome: {{ form.name }}</p>
+  <p>Idade: {{ form.age }}</p>
+  <p>Mensagem: {{ form.message }}</p>
+  <p>Newsletter: {{ form.newsletter }}</p>
+  <p>Cor: {{ form.color }}</p>
+  <p>Fruta: {{ form.fruit }}</p>
 </template>
 
 <script lang="ts">
@@ -12,13 +39,35 @@ import { defineComponent } from "vue"
 export default defineComponent({
   data() {
     return {
-      products: [
-        { id: 1, name: "Camisa", stock: true },
-        { id: 2, name: "Bota", stock: false },
-        { id: 3, name: "Chapéu", stock: false },
-        { id: 4, name: "Cinto", stock: true }
-      ]
+      form: {
+        name: "",
+        age: 0,
+        message: "",
+        newsletter: false,
+        color: "",
+        fruit: ""
+      }
+    }
+  },
+
+  methods: {
+    send() {
+      // axios.post('/register', this.form)
     }
   }
 })
 </script>
+
+<style scoped>
+input[type="email"],
+input[type="text"],
+select,
+textarea {
+  display: block;
+}
+
+form {
+  padding-bottom: 1rem;
+  border-bottom: 2px solid #000;
+}
+</style>
