2692828858fbf858edfb0d1fdf25a44be59080a9
Gustavo Amorim <guh.contato.work@outlook.com.br>
2021-09-14 20:52:04 -0300

    Mode

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,38 @@
 <template>
-  <button @click="show = !show">Toggle</button>
+  <ul>
+    <li @click="view = 'Home'">Home</li>
+    <li @click="view = 'Contact'">Contact</li>
+    <li @click="view = 'About'">About</li>
+  </ul>
+  <!--Para criar isso usei ul>li*3 - mind blowing-->
 
-  <transition enter-active-class="animate__animated animate__sludeInLeft" :appear="show" leave-active-class="animate__animated animate__slideOutLeft"><h1 v-if="show">Cataline</h1></transition>
+  <transition
+    enter-active-class="animate__animated animate__fadeIn"
+    :appear="true"
+    leave-active-class="animate__animated animate__fadeOut"
+    mode="out-in"
+  >
+    <component :is="view" />
+  </transition>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue"
+import Home from "@/pages/Home.vue"
+import About from "@/pages/About.vue"
+import Contact from "@/pages/Contact.vue"
+
 import "animate.css"
 
 export default defineComponent({
+  components: {
+    Home,
+    About,
+    Contact
+  },
   data() {
     return {
-      show: false
+      view: "Home"
     }
   }
 })
